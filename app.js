71d8c2fbbf1e456b59abc8766b9f133d0d6fe9d9
Darkzero7 <chaiyanituporn@gmail.com>
2024-05-19 21:36:33 +0700

    js lab 50

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-// write code here
+let username = prompt("Username : ");
+let email = prompt("Email : ");
+let password = prompt("password : ");
+
+let user = {
+    username: username,
+    email: email,
+    password: password
+}
+
+console.log(user)
